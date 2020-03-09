13752ee172e95a489242fc827e43c5fc4789abcf
Pruthvi5a2 <pruthvirajm392@gmail.com>
2020-03-09 15:07:17 +0530

    Updated

---
+++ BehaviouralPattern/src/main/java/IteratorDemo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+ 
+public class IteratorDemo {
+ 
+  public static void main(String args[]){
+    ArrayList names = new ArrayList();
+    names.add("Chaitanya");
+    names.add("Steve");
+    names.add("Jack");
+ 
+    Iterator it = names.iterator();
+ 
+    while(it.hasNext()) {
+      String obj = (String)it.next();
+      System.out.println(obj);
+    }
+  }
+}
